00cb6b1a37a058a639be5a1f0df16c3ed3c9bd81
E ESWAR BABU <eswarbabu.e23@iiits.in>
2025-11-01 22:59:00 +0530

    Add Recruiter index.js exports and Recruiter.css styles

---
+++ client/src/components/Recruiter/index.js
@@ -0,0 +1,8 @@
+// Recruiter Components - Index Export
+export { default as RecruiterHome } from './RecruiterHome';
+export { default as RecruiterDashboard } from './RecruiterDashboard';
+export { default as RecruiterJobs } from './RecruiterJobs';
+export { default as RecruiterApplications } from './RecruiterApplications';
+export { default as RecruiterNotifications } from './RecruiterNotifications';
+export { default as RecruiterProfile } from './RecruiterProfile';
+export { default as RecruiterNavbar } from './RecruiterNavbar';
